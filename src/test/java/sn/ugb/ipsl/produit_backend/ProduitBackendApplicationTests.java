bc915b56778b40e075c7836bee2a06cbb0870189
Xhadee <khadiabsatou@gmail.com>
2026-02-19 16:55:16 +0100

    Initialisation : Structure Spring Boot et dépendances

---
+++ src/test/java/sn/ugb/ipsl/produit_backend/ProduitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ugb.ipsl.produit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
